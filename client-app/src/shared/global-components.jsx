aeb48f38059bed1114453a43003a28a94f9d2a50
sidewndr <anoshkinzz@mail.ru>
2021-03-18 15:31:20 +0300

    Add: Media queries

--- client-app/src/shared/global-components.jsx
+++ client-app/src/shared/global-components.jsx
@@ -36,4 +36,49 @@ export default createGlobalStyle`
   body {
     margin: 0;
   }
+  
+  
+  //ADAPTIVE MEDIA QUERIES
+
+  //Desktop >1600px styles
+  @media screen and (min-width: 1600px) {
+    html {
+      font-size: 16px;
+    }
+    body {
+      background-color: red;
+    }
+  }
+
+  //Desktop 1440-1599px styles
+  @media screen and (min-width: 1200px) and (max-width: 1599px){
+    html {
+      font-size: 14.4px;
+    }
+    body {
+      //background-color: blue;
+    }
+  }
+
+  //Desktop 1200px-1439px styles
+  @media screen and (min-width: 1200px) and (max-width: 1439px){
+    html {
+      font-size: 12px;
+    }
+    body {
+      //background-color: yellow;
+    }
+  }
+
+  //Desktop 1200px-1439px styles
+  @media screen and (min-width: 1200px) and (max-width: 1439px){
+    html {
+      font-size: 12px;
+    }
+    body {
+      //background-color: yellow;
+    }
+  }
 `
+
+//TODO: Media queries
