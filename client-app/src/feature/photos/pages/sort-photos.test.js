7270cd0cf607eb551418142fa8e4a5efbf9f7c76
sidewndr <anoshkinzz@mail.ru>
2021-03-29 23:33:40 +0200

    [TESTS]: add unit test for photo sort algorithm

--- client-app/src/feature/photos/pages/sort-photos.test.js
+++ client-app/src/feature/photos/pages/sort-photos.test.js
@@ -1,33 +1,41 @@
 import {sortPhotos} from "./sort-photos"
 
+const smallPhoto = {
+  link: '',
+  size: 'small'
+}
 
+const largePhoto = {
+  link: '',
+  size: 'large'
+}
 
 
 test('PhotosSort (default) test', () => {
-  const inputData = [1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2]
-  const outputData = [1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2]
+  const inputData = [smallPhoto, smallPhoto, smallPhoto, smallPhoto, largePhoto, largePhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, largePhoto, largePhoto]
+  const outputData = [smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto]
 
   expect(sortPhotos(inputData)).toEqual(outputData)
 })
 
 
-test('PhotosSort (more 2) test', () => {
-  const inputData = [1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
-  const outputData = [1, 1, 2, 1, 1, 2, 2, 2, 2, 2]
+test('PhotosSort (more largePhoto) test', () => {
+  const inputData = [smallPhoto, smallPhoto, smallPhoto, smallPhoto, largePhoto, largePhoto, largePhoto, largePhoto, largePhoto, largePhoto]
+  const outputData = [smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, largePhoto, largePhoto, largePhoto, largePhoto]
 
   expect(sortPhotos(inputData)).toEqual(outputData)
 })
 
-test('PhotosSort (more 2, excess 1) test', () => {
-  const inputData = [1, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2]
-  const outputData = [1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 2, 2, 2, 1]
+test('PhotosSort (more largePhoto, excess smallPhoto) test', () => {
+  const inputData = [smallPhoto, largePhoto, smallPhoto, smallPhoto, smallPhoto, largePhoto, largePhoto, largePhoto, smallPhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, largePhoto, largePhoto, largePhoto, largePhoto]
+  const outputData = [smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, largePhoto, largePhoto, largePhoto, smallPhoto]
 
   expect(sortPhotos(inputData)).toEqual(outputData)
 })
 
-test('PhotosSort (more 1) test', () => {
-  const inputData = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2]
-  const outputData = [1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1]
+test('PhotosSort (more smallPhoto) test', () => {
+  const inputData = [smallPhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, largePhoto, largePhoto, largePhoto]
+  const outputData = [smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, largePhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto, smallPhoto]
 
   expect(sortPhotos(inputData)).toEqual(outputData)
 })
