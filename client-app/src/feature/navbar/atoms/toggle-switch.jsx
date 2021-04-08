4b2627a8b7fd64ff18d9bf74144f18ce9ef9c4e2
sidewndr <anoshkinzz@mail.ru>
2021-04-09 00:17:10 +0300

    Add: fix toggle-switch

--- client-app/src/feature/navbar/atoms/toggle-switch.jsx
+++ client-app/src/feature/navbar/atoms/toggle-switch.jsx
@@ -1,74 +1,38 @@
 import React  from "react";
 import styled from "styled-components";
 
-const ToggleSwitchStl = styled.div`
+
+const ToggleSwitchStl = styled.input`
   position: relative;
+
   width: 4.375rem;
   height: 2.1875rem;
-
-  //Mobile styles
-  @media screen and (max-width: 576px){
-    width: 2.25rem;
-    height: 1.125rem;
-  }
-`
-
-const CheckBoxStl = styled.input`
-  display: none;
-  
-  &:checked + label {
-    //background-color: ;
-    &::after{
-      margin-left: 2.1875rem;
-      
-      content: "";
-      display: block;
-      width: 2.1875rem;
-      height: 2.1875rem;
-      border: #FFFFFF 0.125rem solid;
-      border-radius: 1.125rem;
-      background-color: #133F2C;
-      transition: .3s;
-
-      //Mobile styles
-      @media screen and (max-width: 576px){
-        margin-left: 1.125rem;
-
-        width: 1.125rem;
-        height: 1.125rem;
-        border: #FFFFFF 0.0643rem solid;
-        border-radius: 0.5786rem;
-      }
-    }
-}
-`
-
-const SliderStl = styled.label`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 4.375rem;
-  height: 2.1875rem;
-  border-radius: 1.125rem;
+  appearance: none;
   background-color: #FFFFFF;
-  cursor: pointer;
+  border-radius: 1.125rem;
+  outline: none;
+  transition: 0.3s;
 
   //Mobile styles
   @media screen and (max-width: 576px){
     width: 2.25rem;
     height: 1.125rem;
-    border-radius: 0.5786rem;
   }
-  
-  &::after{
+
+  :checked{
+    //background-color: ;
+  }
+
+  ::before{
+    position: absolute;
+
     content: "";
-    display: block;
     width: 2.1875rem;
     height: 2.1875rem;
     border: #FFFFFF 0.125rem solid;
     border-radius: 1.125rem;
     background-color: #133F2C;
-    transition: .3s;
+    transition: 0.3s;
 
     //Mobile styles
     @media screen and (max-width: 576px){
@@ -78,18 +42,20 @@ const SliderStl = styled.label`
       border-radius: 0.5786rem;
     }
   }
+
+  :checked::before{
+    transform: translateX(2.1875rem);
+
+    //Mobile styles
+    @media screen and (max-width: 576px){
+      transform: translateX(1.125rem);
+    }
+  }
 `
 
 
 export const ToggleSwitch = ({onToggle}) => {
   return (
-    <ToggleSwitchStl>
-      <CheckBoxStl type={"checkbox"} id={"checkbox"}/>
-
-      <SliderStl
-        htmlFor={"checkbox"}
-        onClick={onToggle}
-      />
-    </ToggleSwitchStl>
+    <ToggleSwitchStl type={"checkbox"} onClick={onToggle} />
   )
 }
