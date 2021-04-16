b19a28626b920b25869adf13b586947466538507
sidewndr <anoshkinzz@mail.ru>
2021-04-16 22:22:40 +0200

    [ADD]: init Home template

--- client-app/src/feature/preview/desktop/templates/home-temp.jsx
+++ client-app/src/feature/preview/desktop/templates/home-temp.jsx
@@ -2,18 +2,41 @@ import React from 'react'
 import styled from 'styled-components'
 import {Column, Container} from "../../../../shared/layout";
 
-const NavbarContainer = styled(Container)`
+const NavbarContainerStl = styled(Container)`
   height: 6.481vh;
+  background-color: #ccc;
+  
+  margin-bottom: 1vh;
+`
+
+const LabelContainerStl = styled(Container)`
+  height: 12.05vh;
+  background-color: #ccc;
+  
+  margin-bottom: 4.25vh;
+`
+
+const MainContentContainerStl = styled(Container)`
+  height: 67.5vh;
+  background-color: #ccc;
+
+  margin-bottom: auto;
+`
+
+const FooterContainerStl = styled(Container)`
+  height: 6.48vh;
   background-color: #ccc;
 `
 
 
-export const HomeTemp = ({}) => {
-
+export const HomeTemp = ({navbarChildren, labelChildren, mainContentChildren, footerChildren}) => {
   return (
     <>
-      <Column>
-        <NavbarContainer />
+      <Column h100>
+        <NavbarContainerStl>{navbarChildren}</NavbarContainerStl>
+        <LabelContainerStl>{labelChildren}</LabelContainerStl>
+        <MainContentContainerStl>{mainContentChildren}</MainContentContainerStl>
+        <FooterContainerStl>{footerChildren}</FooterContainerStl>
       </Column>
     </>
   )
