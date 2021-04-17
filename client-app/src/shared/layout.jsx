d7f65c424b070e67408058895d87090dd2c6a5a3
sidewndr <anoshkinzz@mail.ru>
2021-04-17 23:16:19 +0200

    [ADD]: Layout components

--- client-app/src/shared/layout.jsx
+++ client-app/src/shared/layout.jsx
@@ -17,13 +17,25 @@ export const Column = styled.div`
 `
 
 export const Container = styled.div`
+  ${props => props.flexRowCenter && css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  `}  
   
+  ${props => props.flexColumnCenter && css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `}
 `
 
 export const Wrapper = styled.div`
   ${props => props.w1600 && css`
-    //width: 148.1481vh;
-    width: 100%;
+    width: 148.1481vh;
+    //width: 100%;
     max-width: 1600px;
   `}
 `
