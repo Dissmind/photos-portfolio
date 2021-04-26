b293c1daa074d7d79baa448e13d470bd8ab2ec60
sidewndr <anoshkinzz@mail.ru>
2021-04-26 21:18:35 +0200

    [ADD]: fix styles

--- client-app/src/feature/photos/desktop/molecules/main-content.jsx
+++ client-app/src/feature/photos/desktop/molecules/main-content.jsx
@@ -1,32 +1,50 @@
 import React, {useEffect} from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 import {Container, Wrapper} from "../../../../shared/layout";
 import {useDispatch, useSelector} from "react-redux";
-import {selectPhotosLinks, setPhotos} from "../../photos.slice";
+import {selectPhotosLinks, selectSmallPhotosCount, setPhotos} from "../../photos.slice";
 import {sortPhotos} from "../pages/sort-photos";
 import {getPhotosMock} from "../../../../tests/mocks/photosMock";
 import {getTitleFromUrl} from "../atoms/title";
 import {ThemeListItems} from "../../../portfolio/desktop/molecules/theme-link-list";
+import {media} from "../../../../shared/media-queries";
 
 const MainContentStl = styled(Container)`
   display: grid;
   grid-template-columns: repeat(2, min-content);
+  place-items: center;
+  justify-content: center;
+  grid-gap: 1.8518vh;
   
   height: 100%;
   
-  //place-items: center;
-  justify-content: center;
+  ${media.desktop} {
+    grid-gap: 1.25vw;
+  }
 `
 
-const SmallPhotoWrapperStl = styled(Wrapper)`
-  width: 36.25rem;
-  height: 47.5rem;
-`
-
-const LargePhotoWrapperStl = styled(Wrapper)`
-  width: 73.75rem;
-  height: 45rem;
-  grid-column: 1/3;
+const PhotoWrapperStl = styled(Wrapper)`
+  ${props => props.small && css`
+    width: 53.7037vh;
+    height: 70.3703vh;
+    grid-column: ${props => props.lastPhoto ? '1 / 3' : 'auto'};
+    
+    ${media.desktop} {
+      width: 36.25vw;
+      height: 47.5vw;
+    }
+  `}
+  
+  ${props => props.large && css`
+    width: 109.2592vh;
+    height: 66.6666vh;
+    grid-column: 1 / 3;
+    
+    ${media.desktop} {
+      width: 73.75vw;
+      height: 45vw;
+    }
+  `}
 `
 
 const PhotoStl = styled.img`
@@ -39,6 +57,7 @@ const PhotoStl = styled.img`
 export const MainContent = ({}) => {
 
   const photosLinks = useSelector(selectPhotosLinks)
+  const smallPhotoCount = useSelector(selectSmallPhotosCount)
   const dispatch = useDispatch()
 
   const groupTitleParsed = getTitleFromUrl(window.location.href, ThemeListItems)
@@ -48,20 +67,47 @@ export const MainContent = ({}) => {
     dispatch(setPhotos({photos}))
   }, [])
 
+
   return (
     <MainContentStl limiter>
-      {
-        photosLinks.map(i => (
-          i.size === "small"
-            ? (<SmallPhotoWrapperStl>
-                <PhotoStl src={i.link} />
-              </SmallPhotoWrapperStl>)
 
-            : (<LargePhotoWrapperStl>
-                <PhotoStl src={i.link} />
-              </LargePhotoWrapperStl>)
-        ))
+      {
+        photosLinks.map((item, index) => {
+          if (item.size === 'small') {
+            if (index === photosLinks.length - 1) {
+              if ((smallPhotoCount - 1) % 2 === 1) {
+                return(
+                  <PhotoWrapperStl small>
+                    <PhotoStl src={item.link} />
+                  </PhotoWrapperStl>
+                )
+              }
+              else {
+                return(
+                  <PhotoWrapperStl small lastPhoto>
+                    <PhotoStl src={item.link} />
+                  </PhotoWrapperStl>
+                )
+              }
+            }
+            else {
+              return(
+                <PhotoWrapperStl small>
+                  <PhotoStl src={item.link} />
+                </PhotoWrapperStl>
+              )
+            }
+          }
+          else {
+            return(
+              <PhotoWrapperStl large>
+                <PhotoStl src={item.link} />
+              </PhotoWrapperStl>
+            )
+          }
+        })
       }
+
     </MainContentStl>
   )
 }
